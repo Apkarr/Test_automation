0a993f537f70c3a76d2556d0cf05b4ca29e395f9
Артём <97799751+Apkarr@users.noreply.github.com>
2022-06-03 18:14:08 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+
+valid_email = "artemapkarov108@gmail.com"
+valid_password = "lecanuki"
+
+val_id = "123"
+val_name = "Смолин Петр Михайлович"
+val_phone = "79001234567"
+val_email = "ivanov@site.com"
